5192c7989c2812de7f4763d2ac6b58411ae11bb8
Ishini Halgaswatta <ishini.hal@gmail.com>
2020-09-18 21:02:43 +0530

    delivery manager account ui is done

---
+++ app/src/main/java/com/example/techmart/ishini/DM_Add_DP.java
@@ -0,0 +1,15 @@
+package com.example.techmart.ishini;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.techmart.R;
+
+public class DM_Add_DP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d_m__add__d_p);
+    }
+}
---
+++ app/src/main/java/com/example/techmart/ishini/DM_My_Account.java
@@ -0,0 +1,16 @@
+package com.example.techmart.ishini;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.techmart.R;
+
+public class DM_My_Account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_d_m__my__account);
+    }
+}
