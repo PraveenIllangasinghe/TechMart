e676c4a50e46d26aeaca9cc10c3daad34882e91d
DileepaNaveen99 <naveenillangasinghe99@gmail.com>
2020-09-25 13:18:16 +0530

    Seller sign up added

--- app/src/main/java/com/example/techmart/naveen/AddProduct.java
+++ app/src/main/java/com/example/techmart/naveen/AddProduct.java
@@ -3,12 +3,14 @@ package com.example.techmart.naveen;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.techmart.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,7 +21,7 @@ public class AddProduct extends AppCompatActivity {
 
 
     EditText id,name,price,des;
-    Button addProd;
+    Button addProd, logOutSellerBtn;
     DatabaseReference ref;
     long max = 0;
     Product Prod;
@@ -69,6 +71,17 @@ public class AddProduct extends AppCompatActivity {
 
              ref.child(String.valueOf(max+1)).setValue(Prod);
 
+         }
+     });
+
+
+     logOutSellerBtn = findViewById(R.id.sellerSignUpOutbtn);
+     logOutSellerBtn.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             FirebaseAuth.getInstance().signOut();
+             Intent intentsignout = new Intent(AddProduct.this, SignUpSeller.class);
+             startActivity(intentsignout);
          }
      });
 
